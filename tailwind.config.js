2aa395b2b0433fb8a5d9c60a3f3266165917eabb
Παπαδήμας Ζωγράφος <z.papadhmas@modus.gr>
2026-01-02 12:31:44 +0200

    style: apply Roboto Flex and Lora fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"Roboto Flex"', 'sans-serif'],
+        serif: ['Lora', 'serif'],
+      },
+    },
   },
   plugins: [],
 }
